cbaf10f33ac27a8a94fb6e0c1cd787a956cd521e
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2020-10-06 11:12:58 -0400

    final version 1.0

---
+++ index.js
@@ -0,0 +1,37 @@
+"use strict";
+
+function getDogImage(breedType) {
+    console.log(breedType);
+  fetch(`https://dog.ceo/api/breed/${breedType}/images/random`)
+    .then((response) => response.json())
+    .then((responseJson) => displayResults(responseJson))
+    .catch((error) => alert("Something is wrong, try again."));
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  //replace the existing image with the new one
+    if (responseJason.status === 200) {
+  $(".results").append(
+    `<img src="${responseJson.message}" class="image-results">`
+  );
+  $(".image-results").removeClass('hidden');
+    }
+    else {
+        alert('No breed found, please try again');
+    }
+};
+
+function watchForm() {
+  $("form").submit((event) => {
+    event.preventDefault();
+    let breedInput = $('input[name="breedInput"]').val();
+    console.log(breedInput);
+    getDogImage(breedInput);
+  });
+};
+
+$(function () {
+  console.log("App loaded! Waiting for submit!");
+  watchForm();
+});
